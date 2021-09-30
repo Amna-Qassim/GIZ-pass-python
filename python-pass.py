74426af2b99de64dfb2cbfcb5dbe6f62515cf48d
Amna-Qassim <amnakassim6@gmail.com>
2021-10-01 00:20:07 +0300

    solution of task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,28 @@
-
-
 class Solution:
+    def longestPalindrome(self, s):
+        longest = ""
+        i = 0
+        l = len(s)
+
+        while i < l:
+            end = i
+
+            while end + 1 < l and s[end + 1] == s[i]:
+                end += 1
+
+            candidate = self.get_palindrome(s, i, end)
+
+            if len(candidate) > len(longest):
+                longest = candidate
+
+            i = end + 1
+
+        return longest
 
     @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+    def get_palindrome(s, start, end):
+        while start > 0 and end + 1 < len(s) and s[start - 1] == s[end + 1]:
+            start -= 1
+            end += 1
+
+        return s[start:end + 1]
